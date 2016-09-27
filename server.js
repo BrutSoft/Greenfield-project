e8f716c84332836824eedefe81f8e69c3cdaf53d
Nathan Donolli <ndonolli@gmail.com>
2016-09-27 15:27:16 -0500

    add bodyparser to express.use()

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ var app = express();
 var PORT = process.env.PORT || '3000';
 
 app.use(express.static('public'));
+app.use(bodyParser.json());
 
 app.listen(PORT, function() {
   console.log('Listening on port ' + PORT);
