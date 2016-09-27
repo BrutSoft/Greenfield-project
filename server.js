f83fd572b8afae6b929086dfede4fb271d13a2b4
Nathan Donolli <ndonolli@gmail.com>
2016-09-27 15:58:42 -0500

    add morgan, and changes to middleware

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var morgan = require('morgan');
 
 var app = express();
 
 var PORT = process.env.PORT || '3000';
 
+// Middleware setup
+app.use(morgan('dev'));
 app.use(express.static('public'));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
+// Hey! Listen! Hey!
 app.listen(PORT, function() {
   console.log('Listening on port ' + PORT);
 });
