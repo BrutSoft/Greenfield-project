9cf80ec5b0f2175c32842e4850c838d61e2f49e5
Nathan Donolli <ndonolli@gmail.com>
2016-09-28 11:35:04 -0500

    webpack config works with babel, add entry point main.js, install dependencies for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,29 @@
+var debug = process.env.NODE_ENV !== "production";
+var webpack = require('webpack');
+
 module.exports = {
-    // NOT SURE ABOUT THIS YET
-    //
-    // entry: './server.js',
-    // output: {
-    //     path: './dist',
-    //     filename: 'bundle.js'
-    // },
-    module: {
-        preLoaders: [
-            {
-                test: /\.jsx?$/,
-                exclude: /node_modules/,
-                loader: "jshint-loader"
-            }
-        ]
-    }
+  context: __dirname,
+  devtool: debug ? "inline-sourcemap" : null,
+  entry: __dirname + "/public/main.js",
+  output: {
+    path: __dirname + "/public/dist/",
+    filename: "bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: /.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  },
+  plugins: debug ? [] : [
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
+  ],
 };
