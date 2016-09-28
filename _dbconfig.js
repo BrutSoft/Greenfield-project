fdd4f3b7b9fe67aa03a6587f06c16a20e602bb69
David d'Aquin <david@daquin.com>
2016-09-28 14:38:12 -0500

    Created dbconfig sample file

---
+++ _dbconfig.js
@@ -0,0 +1,18 @@
+var dbAuth = {};
+
+dbAuth.username = 'REPLACE ME';//replace with your username. Likely root. 
+dbAuth.password = 'REPLACE ME';//replace with your password that you set up for your local mysql. 
+
+module.exports = dbAuth;
+
+/**
+ * 
+ * Instructions: 
+ * 
+ * First, resave this file without the underscore ('_'). 
+ * 
+ * Then, replace the above with your own username and password. 
+ * 
+ * It should work after that. Restart your node server and see if you get the successful connect message. 
+ * 
+ */
