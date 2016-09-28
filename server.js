becc0739a21b09f048b56324a1fdb1978ae7fcba
David d'Aquin <david@daquin.com>
2016-09-28 15:34:40 -0500

    removed commented out database set up

--- server.js
+++ server.js
@@ -16,32 +16,6 @@ app.use(bodyParser.urlencoded({
 
 // Database set up ================================
 
-// var Sequelize = require('sequelize');
-// var dbAuth = require('./dbconfig');
-
-// var sequelize = new Sequelize('donation_page', dbAuth.username, dbAuth.password, {
-//   host: 'localhost',
-//   dialect: 'mysql',
-
-//   pool: {
-//     max: 5,
-//     min: 0,
-//     idle: 10000
-//   },
-// });
-
-// sequelize
-//   .authenticate()
-//   .then(function(err) {
-//     console.log('Connection has been established successfully.');
-//   })
-//   .catch(function (err) {
-//     console.log('Unable to connect to the database:', err);
-//   });
-
-
-// module.exports = sequelize; 
-
 var db = require('./database.js');
 
 // End Database Set up ============================
