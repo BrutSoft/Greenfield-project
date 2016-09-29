ac30239b622ee332e9ab042c30751492f8026a51
Zivile <zivilytez@hotmail.com>
2016-09-29 01:15:14 -0500

    changed Navbar component

--- public/components/navbar/Navbar.jsx
+++ public/components/navbar/Navbar.jsx
@@ -1,18 +1,51 @@
 import React from 'react';
-import Login from './Login.jsx';
-import Admin from './Admin.jsx';
-import Logout from './Logout.jsx';
-import Register from './Register.jsx';
+import { Router, Link } from 'react-router';
+import * as firebase from 'firebase';
 
-export default class Navbar extends React.Component {
+var Navbar = React.createClass({
+  getInitialState() {
+      return {
+          loggedIn: (null !== firebase.auth().currentUser)  
+      };
+  },
+  componentWillMount() {
+      firebase.auth().onAuthStateChanged(firebaseUser => {
+        this.setState({
+          loggedIn: (null !== firebaseUser)
+        })
+        if (firebaseUser) {
+          console.log('Logged in ', firebaseUser);
+        } else {
+          console.log('Not logged in');
+        }
+      }); 
+  },
   render () {
+    var loginOrOut;
+    var register;
+    if (this.state.loggedIn) {
+      loginOrOut = <li><Link to='/logout'><button>Logout</button></Link></li>;
+      register = null
+    } else {
+      loginOrOut = <li><Link to='/login'><button>Login</button></Link></li>;
+      register = <li><Link to='/register'><button>Register</button></Link></li>;
+    }
     return (
-      <nav>
-        <Login />
-        <Register />
-        <Admin />
-        <Logout />
-      </nav>
-    );
+      <span>
+        <nav className='navbar'>
+          <div className='container'>
+            <div className='navbar-title'>
+              <h1>OPERATION SPARK DONATION PAGE</h1>
+            </div>
+            <ul>
+              {register}
+              {loginOrOut}
+            </ul>
+          </div>
+        </nav>
+      </span>
+    )
   }
-};
+});
+
+module.exports = Navbar;
