486dc7acb91b5f07a3aa1e93967cd08d8d0b360e
Nathan Donolli <ndonolli@gmail.com>
2016-09-29 07:48:05 -0500

    change configs, webpack consumes main.js but throws error at app.jsx

--- public/main.js
+++ public/main.js
@@ -1,1 +1,1 @@
-import App from './app.jsx';
+import './app';
--- webpack.config.js
+++ webpack.config.js
@@ -6,27 +6,35 @@ const path = require('path');
 module.exports = {
   context: __dirname,
   devtool: debug ? 'inline-sourcemap' : null,
-  entry: path.join(__dirname, '/public/main.js'),
+  entry: path.resolve(__dirname, 'public/main.js'),
   output: {
-    path: path.join(__dirname, '/public/dist/'),
+    path: path.resolve(__dirname, 'public/dist/'),
     filename: 'bundle.js',
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx'],
   },
   module: {
     loaders: [
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
-        loader: 'react-hot!babel',
+        loader: 'babel',
+        query: {
+          presets: ['es2015'],
+        },
+        include: path.join(__dirname, 'public'),
       },
       {
         test: /\.js$/,
         exclude: /node_modules/,
         loaders: ['babel-loader', 'eslint-loader'],
+        include: path.join(__dirname, 'public'),
       },
     ],
   },
   eslint: {
-    configFile: './.eslintrc',
+    configFile: './.eslintrc.js',
   },
   plugins: debug ? [] : [
     new webpack.optimize.DedupePlugin(),
