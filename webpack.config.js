4f9074f858621a3254d2a93f86517d6c791722f1
Nathan Donolli <ndonolli@gmail.com>
2016-09-28 21:03:20 -0500

    install eslint loader and airbnb rules.  much strict. such trailing comma

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,34 @@
-var debug = process.env.NODE_ENV !== "production";
-var webpack = require('webpack');
+const debug = process.env.NODE_ENV !== 'production';
+const webpack = require('webpack');
+const path = require('path');
+
 
 module.exports = {
   context: __dirname,
-  devtool: debug ? "inline-sourcemap" : null,
-  entry: __dirname + "/public/main.js",
+  devtool: debug ? 'inline-sourcemap' : null,
+  entry: path.join(__dirname, '/public/main.js'),
   output: {
-    path: __dirname + "/public/dist/",
-    filename: "bundle.js"
+    path: path.join(__dirname, '/public/dist/'),
+    filename: 'bundle.js',
   },
   module: {
+    preLoaders: [
+      {
+        test: /\.jsx?$/,
+        loaders: ['eslint'],
+        include: path.join(__dirname, '/public/'),
+      },
+    ],
     loaders: [
       {
         test: /.jsx?$/,
         loader: 'babel-loader',
         exclude: /node_modules/,
         query: {
-          presets: ['es2015', 'react']
-        }
-      }
-    ]
+          presets: ['es2015', 'react'],
+        },
+      },
+    ],
   },
   plugins: debug ? [] : [
     new webpack.optimize.DedupePlugin(),
