38fa5c941f9fb2f8a49715dd81bf8e0a5055fc84
Zivile Januleviciute <zivile.goodwin@gmail.com>
2016-09-29 15:29:34 -0500

    Delete firebase.config.js

--- firebase.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    apiKey: "AIzaSyAswVVbxhXA7jPMVhIyuzMYhvegKntLbPY",
-    authDomain: "op-sp-donation.firebaseapp.com",
-    databaseURL: "https://op-sp-donation.firebaseio.com",
-    storageBucket: "op-sp-donation.appspot.com",
-    messagingSenderId: "119678039306"
-}
