f78f6743187bdd9f37a62403b8c810bac97addad
David d'Aquin <david@daquin.com>
2016-09-30 19:32:17 -0500

    more work done on form

--- public/components/paymentForm/paymentForm.jsx
+++ public/components/paymentForm/paymentForm.jsx
@@ -13,6 +13,13 @@ class PaymentForm extends React.Component {
 
         <div className="form-row">
           <label htmlFor>
+            <span>Donation Amount</span>
+            <input type="text" size="8" />
+          </label>
+        </div>
+
+        <div className="form-row">
+          <label htmlFor>
             <span>Card Number</span>
             <input type="text" size="20" data-stripe="number" />
           </label>
@@ -39,6 +46,44 @@ class PaymentForm extends React.Component {
             <span>Billing Zip</span>
             <input type="text" size="6" data-stripe="address_zip" />
           </label>
+        </div>
+
+        <div className="form-row">
+          <label htmlFor>
+            <span>Email Address</span>
+            <input type="text" size="30" />
+          </label>
+        </div>
+
+        <div className="form-row">
+          <input type="checkbox" value="signUpForEmailList" />
+          <span>Would you like to sign up for our newsletter?</span>
+        </div>
+
+        <div className="form-row">
+          <input type="checkbox" value="getSticker" />
+          <span>Would you like to receive an Operation Spark sticker?</span>
+          { // TODO this should not appear unless donation Amount
+            // is higher than $10(?)
+          }
+        </div>
+
+        <div className="form-row">
+          <input type="checkbox" value="getShirt" />
+          <span>Would you like to receive an Operation Spark T-Shirt?</span>
+          <br />
+          <select name="shirtSize">
+            <option value="small">S</option>
+            <option value="medium">M</option>
+            <option value="large">L</option>
+            <option value="xlarge">XL</option>
+            <option value="xxlarge">XXL</option>
+          </select>
+          <span>Shirt Size</span>
+
+          { // TODO this should not appear unless donation Amount
+            // is higher than $30(?)
+          }
         </div>
 
         <input type="submit" className="submit" value="Submit Payment" />
