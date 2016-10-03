4783949f77bcce22bfaf9f46b4c5f9be4f4c562c
Nathan Donolli <ndonolli@gmail.com>
2016-10-03 14:20:11 -0500

    add react-cardstack

---
+++ public/components/tierInfo.js
@@ -0,0 +1,24 @@
+const lorum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+
+const tiers = [
+  {
+    name: 'Our hosting bill for a month',
+    amount: 25,
+    description: lorum,
+    background: '#E1BC29',
+  },
+  {
+    name: 'Chromebook for a student',
+    amount: 250,
+    description: lorum,
+    background: '#3BB273',
+  },
+  {
+    name: 'Bootcamp for one student',
+    amount: 825,
+    description: lorum,
+    background: '#4D9DE0',
+  }
+];
+
+export default tiers;
