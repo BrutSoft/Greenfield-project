f95701fe893976bb1376ae170ec90864ccca5111
David d'Aquin <david@daquin.com>
2016-10-02 20:23:23 -0500

    Stored away secret key

---
+++ stripe.config.example.js
@@ -0,0 +1,10 @@
+var secretKey = 'PUT THE KEY HERE';
+
+module.exports = secretKey;
+
+/**
+ * Instructions:
+ *
+ *  Create a new file names stripe.config.js and replace the above variable with
+ *  your stripe secret key. 
+ */
